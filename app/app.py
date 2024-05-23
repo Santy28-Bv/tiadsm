b272d2c21d04075f9b8cdb20fb69256f77fc07ec
Santiago <167657885+Santy28-Bv@users.noreply.github.com>
2024-05-23 11:45:15 -0600

    ya commit

---
+++ app/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/about_us")
+def aboutus():
+    return render_template('about_us.html')
+
+
+@app.route("/layout")
+def layout():
+    return render_template('layout.html')
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html')
+
+def pagina_no_encontrada(error):
+    return render_template('404.html'),404
+
+
+if __name__=='__main__':
+    app.register_error_handler(404, pagina_no_encontrada)
+    app.run(debug=True, port=5000)
+    
+
+
+#Activar entorno virtual .venv\Scripts\activate
+#Correr aplicación python app\app.py run
+
